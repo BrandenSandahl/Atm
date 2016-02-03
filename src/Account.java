e49607058f881d4a1b788a9a0219edca7e2aebc6
BrandenSandahl <branden157@gmail.com>
2016-02-03 12:38:04 -0500

    sending initial upload

---
+++ src/Account.java
@@ -0,0 +1,18 @@
+/**
+ * Created by branden on 2/3/16 at 12:36.
+ */
+public class Account {
+
+
+// Set up Vars
+
+
+// Constructors if used
+
+
+/** Start Getters and Setters */
+
+
+/** End of Getters and Setters */
+
+}
